42a1e4652e2a2acd01cc56887eab2d08e61ad807
MaartenGDev <maarten.grootoonk@gmail.com>
2016-08-05 18:13:00 +0200

    Added Channel Sorting

--- public/scripts/example.js
+++ public/scripts/example.js
@@ -1,158 +1,206 @@
 var socket = io('http://localhost:3000');
 
 var Message = React.createClass({
-    render: function(){
-      return (
-          <div className="message">
+	render: function() {
+		return(
+			<div className="message">
                 <p><b>{ this.props.user}:</b><i>{this.props.message}</i></p>
           </div>
-      )
-    }
+		)
+	}
+});
+
+var ChannelTitle = React.createClass({
+	render: function() {
+		if(this.props.title === null) return null;
+
+		return(
+			<h1>{this.props.title}</h1>
+		)
+	}
 });
 
 var Channel = React.createClass({
-    render: function(){
-      return (
-          <li>
+	render: function() {
+		return(
+			<li>
             <div onClick={this.props.onClick} className="channel">
               <p><b>{ this.props.channelName}:</b><i>{this.props.unread}</i></p>
             </div>
           </li>
-      )
-    }
+		)
+	}
 });
 
 var ChannelList = React.createClass({
-  changeChannel: function(channel){
-    this.props.onChannelChange(channel);
-  },
-  render: function(){
-    var counter = 0;
+	changeChannel: function(channel) {
+		this.props.onChannelChange(channel);
+	},
+
+	_addChannel(channelTitle, key, boundClick, channel) {
+		return [<ChannelTitle title = {channelTitle} />, <Channel onClick = {boundClick} channelName={ channel.name } key = {key} unread= { channel.unread } />];
+	},
+	_getChannelTitle: function(foundTypes,channel) {
+
+
+		if(foundTypes.indexOf(channel.type) === -1) {
+			foundTypes.push(channel.type);
+			return channel.type;
+		}
+    return null;
+	},
+	render: function() {
+		var foundTypes = [];
+
     var allChannels = Object.keys(this.props.channels);
+		var channelCounter = 0;
+    var allMessages = [];
+    var messages = {};
 
-    var channels = allChannels.map((channelKey) => {
-    var channel = this.props.channels[channelKey];
 
-    var boundClick = this.changeChannel.bind(this, channel.name);
 
-        counter++;
-        return (
-          <Channel onClick = {boundClick} channelName={ channel.name } key = {counter} unread= { channel.unread }></Channel>
-        )
+		allChannels.forEach((channelKey) => {
+			channelCounter++;
+			var channel = this.props.channels[channelKey];
+			var boundClick = this.changeChannel.bind(this, channel.name);
+      var channelTitle = this._getChannelTitle(foundTypes,channel);
+      var channelType = channel.type;
+
+      if(!messages.hasOwnProperty(channelType)){
+        messages[channelType] = [];
+      }
+      console.log(channelTitle);
+      messages[channelType].push(this._addChannel(channelTitle, channelCounter, boundClick, channel));
+		});
+    Object.keys(messages).forEach((channelKey) => {
+      messages[channelKey].forEach((message) => {
+          allMessages.push(message);
+      });
     });
-    return (
-      <div className ="sidebar">
+    console.log(allMessages);
+		return(
+			<div className ="sidebar">
           <ul>
-            {channels}
+            {allMessages}
           </ul>
       </div>
-    );
-  }
+		);
+	}
 });
 var MessageList = React.createClass({
-  render: function(){
-    var counter = 0;
-    var messageNodes = this.props.messages.map(function(message) {
+	render: function() {
+		var counter = 0;
+		var messageNodes = this.props.messages.map(function(message) {
 
-      counter++;
-        return (
-                <Message user = { message.user } key= { counter } message = { message.message }> { message.message }
+			counter++;
+			return(
+				<Message user = { message.user } key= { counter } message = { message.message }> { message.message }
                 </Message>
-            );
+			);
 
-    });
-    return (
-      <div className="chatWindow">
+		});
+		return(
+			<div className="chatWindow">
           {messageNodes}
       </div>
-    )
-  }
+		)
+	}
 });
 var ReactApp = React.createClass({
-    getInitialState: function getInitialState() {
-        return {
-          messages: {
-            currentChannel: "global",
-            channels: {
-              Global: {
-                name: 'Global',
-                messages: [],
-                unread: 0
-              }
-            }
-          }
-        };
+	getInitialState: function getInitialState() {
+		return {
+			messages: {
+				currentChannel: "global",
+				channels: {
+					Global: {
+						type: 'channel',
+						name: 'Global',
+						messages: [],
+						unread: 0,
+					}
+				}
+			}
+		};
 
-    },
-    componentDidMount: function componentDidMount() {
-        socket.emit('chat connect', {
-            token: 'randomOtherId'
-        });
-        socket.on('chat message', this._addMessage);
-    },
-    _setChannel(channel){
-      var currentState = this.state;
+	},
+	componentDidMount: function componentDidMount() {
+		socket.emit('chat connect', {
+			token: 'randomOtherId'
+		});
+		socket.on('chat message', this._addMessage);
+	},
+	_setChannel(channel) {
+		var currentState = this.state;
 
-      currentState.messages.currentChannel = channel;
-      currentState.messages.channels[channel].unread  = 0;
-      this.setState(currentState);
-    },
-    _addMessage: function addMessage(message) {
-        var data = this.state;
-        var channelKey = message.channel;
+		currentState.messages.currentChannel = channel;
+		currentState.messages.channels[channel].unread = 0;
+		this.setState(currentState);
+	},
+	_addMessage: function addMessage(message) {
+		var data = this.state;
+		var channelKey = message.channel;
 
-        if(channelKey == 'From' || channelKey == 'To'){
-          channelKey = message.user;
-        }
-        if(channelKey == ''){
-          channelKey = 'global';
-        }
+		if(channelKey == 'From' || channelKey == 'To') {
+			channelKey = message.user;
+		}
+		if(channelKey == '') {
+			channelKey = 'global';
+		}
 
-        data = this._addMessageToChannel(data,channelKey,message);
+		data = this._addMessageToChannel(data, channelKey, message);
 
-        this.setState(data);
+		this.setState(data);
 
 
-    },
-    _addMessageToChannel: function addMessageToChannel(data,channelKey,message){
-      if(!data.messages.channels.hasOwnProperty(channelKey)){
-        data.messages.channels[channelKey] = {
-          name: channelKey,
-          messages: [],
-          unread: 0
-        } ;
-      }
+	},
+	_addMessageToChannel: function addMessageToChannel(data, channelKey, message) {
 
-      data.messages.channels[channelKey].messages.push(message);
+		var channelType = 'channel';
+		if(message.team !== '') {
+			channelKey = message.team;
+			channelType = 'team';
+		}
 
-      var unreadMessages = data.messages.channels[channelKey].unread;
+		if(!data.messages.channels.hasOwnProperty(channelKey)) {
+			data.messages.channels[channelKey] = {
+				name: channelKey,
+				type: channelType,
+				messages: [],
+				unread: 0
+			};
+		}
 
-      if(data.messages.currentChannel != channelKey){
-        data.messages.channels[channelKey].unread = unreadMessages + 1;
-      }
 
-      return data;
-    },
-    render: function() {
-        var currentChannel = this.state.messages.currentChannel;
-        var channels = this.state.messages.channels;
-        var messages = [];
 
-        if(channels.hasOwnProperty(currentChannel)){
-          messages = channels[currentChannel].messages;
-        }
+		data.messages.channels[channelKey].messages.push(message);
 
-        return (
-            <div className = "ReactApp" >
-                <ChannelList currentChannel={ currentChannel } onChannelChange = { this._setChannel } channels= {this.state.messages.channels} />
+		var unreadMessages = data.messages.channels[channelKey].unread;
+
+		if(data.messages.currentChannel != channelKey) {
+			data.messages.channels[channelKey].unread = unreadMessages + 1;
+		}
+
+		return data;
+	},
+	render: function() {
+		var currentChannel = this.state.messages.currentChannel;
+		var channels = this.state.messages.channels;
+		var messages = [];
+
+		if(channels.hasOwnProperty(currentChannel)) {
+			messages = channels[currentChannel].messages;
+		}
+		return(
+			<div className = "ReactApp" >
+                <ChannelList currentChannel={ currentChannel } onChannelChange = { this._setChannel } channels= {channels} />
                 <MessageList messages = {messages} />
             </div>
-        );
-    }
+		);
+	}
 });
 
-ReactDOM.render( <ReactApp channel = "Party" /> ,
-    document.getElementById('content')
+ReactDOM.render(<ReactApp channel = "Party" />,
+	document.getElementById('content')
 );
 
 socket.emit('chat message', {
@@ -174,5 +222,19 @@ socket.emit('chat message', {
 	channel: 'Guild',
 	user: 'Craftacar',
 	team: '',
+	rank: 'VIP+'
+});
+socket.emit('chat message', {
+	message: 'Oi Holy_Sheep',
+	channel: '',
+	user: 'Randy123',
+	team: 'teamABC',
+	rank: ''
+});
+socket.emit('chat message', {
+	message: 'Oi Holy_Sheep',
+	channel: 'Guild',
+	user: 'Craftacar',
+	team: 'teamABC',
 	rank: 'VIP+'
 });
